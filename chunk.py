22cb56a2dc8d295f65197025f2869be29e5438cc
Peter Hefley <peter.hefley@gmail.com>
2025-07-08 15:20:14 -0500

    Add the chunker.

---
+++ chunk.py
@@ -0,0 +1,38 @@
+import sys
+
+INFILENAME = sys.argv[1]
+OUTFILENAME = sys.argv[2]
+WIDTH = 40
+
+infile = open(INFILENAME, 'rb')
+data = infile.read()
+length = len(data)
+
+
+def chunkToStr(chunk):
+    chunkstr = ""
+    for byte in chunk:
+        thisByteStr = str(hex(byte))[2:]
+        if len(thisByteStr) < 2:
+            thisByteStr = '0'+thisByteStr
+        chunkstr += '\\x' + thisByteStr
+    return chunkstr
+
+start = 0
+outstring = 'unsigned char shellcode[] = '
+while ((start + WIDTH) < length):
+    chunk = data[start:start+WIDTH]
+    chunkstr = chunkToStr(chunk)
+    outstring += '"' + chunkstr + '"\n'
+    start += WIDTH
+
+# one last chunk
+chunk = data[start:]
+chunkstr = chunkToStr(chunk)
+outstring += '"' + chunkstr + '";\n'
+
+print(outstring)
+
+outfile = open(OUTFILENAME, "w")
+outfile.write(outstring)
+outfile.close()
